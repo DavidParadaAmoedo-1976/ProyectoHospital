75893c2e48fea027e36f96c6295b873e7bcc5a9f
DavidParadaAmoedo-1976 <davidparadadam@gmail.com>
2025-11-01 01:32:42 +0100

    avanzando

--- src/DAO/EspecialidadesPostgreDAO.java
+++ src/DAO/EspecialidadesPostgreDAO.java
@@ -2,9 +2,7 @@ package DAO;
 
 import Conexiones.ConexionPostgreSQL;
 import Modelo.EspecialidadesPostgre;
-
 import java.sql.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,8 +10,7 @@ public class EspecialidadesPostgreDAO{
 
 
     public void crear(EspecialidadesPostgre especialidadesPostgre) {
-        String sql = "INSERT INTO hospital.especialidades (nombre_especialidad)" +
-                "VALUES (?)";
+        String sql = "Insert Into hospital.especialidades (nombre_especialidad) Values (?)";
 
         try (Connection conn = ConexionPostgreSQL.getInstancia().getConexion();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -26,12 +23,11 @@ public class EspecialidadesPostgreDAO{
         } catch (SQLException e) {
             System.err.println("Error al insertar especialidad: " + e.getMessage());
         }
-
     }
 
     public List<EspecialidadesPostgre> leerTodos() {
         List<EspecialidadesPostgre> lista = new ArrayList<>();
-        String sql = "SELECT id_especialidad, nombre_especialidad FROM hospital.especialidades";
+        String sql = "Select id_especialidad, nombre_especialidad From hospital.especialidades";
 
         try (Connection conn = ConexionPostgreSQL.getInstancia().getConexion();
              Statement st = conn.createStatement();
@@ -51,7 +47,7 @@ public class EspecialidadesPostgreDAO{
     }
 
     public int obtenerIdPorNombre(String nombre) {
-        String sql = "SELECT id_especialidad FROM hospital.especialidades WHERE nombre_especialidad = ?";
+        String sql = "Select id_especialidad From hospital.especialidades Where nombre_especialidad = ?";
         try (Connection conn = ConexionPostgreSQL.getInstancia().getConexion();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, nombre);
@@ -65,5 +61,4 @@ public class EspecialidadesPostgreDAO{
         }
         return -1;
     }
-
 }
